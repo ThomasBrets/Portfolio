68ba3c4bc60e509b62004d25c3b66ae60d42a3ff
ThomasBrets <thomas15501331@gmail.com>
2025-11-07 23:04:21 -0300

    fix long 6

--- src/App.jsx
+++ src/App.jsx
@@ -7,23 +7,24 @@ import Education from "./components/Education";
 import Contact from "./components/Contact";
 function App() {
   return (
-    <div className="relative overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
-      {/* Fondo único */}
-      <div className="absolute top-0 left-0 -z-10 min-h-full w-full bg-neutral-950 bg-[radial-gradient(ellipse_100%_100%_at_50%_0%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+    <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
+      <div className="fixed top-0 -z-10 h-full w-full">
+      <div className="fixed top-0 left-0 -z-10 min-h-screen w-full bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
 
-      {/* Contenido */}
+      </div>
+     {/* Fondo aplicado a toda la página */}
+     
       <div className="container mx-auto px-8">
-        <Navbar />
-        <Hero />
-        <About />
-        <Technologies />
-        <Projects />
-        <Education />
-        <Contact />
+      <Navbar />
+      <Hero/>
+      <About/>
+      <Technologies/>
+      <Projects/>
+      <Education/>
+      <Contact/>
       </div>
     </div>
   );
 }
-
 
 export default App;
