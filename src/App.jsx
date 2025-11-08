a50c1dd6f9442d704f466e97dcdd5f9aeee66881
ThomasBrets <thomas15501331@gmail.com>
2025-11-07 22:42:20 -0300

    fix long 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,27 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
-import About from "./components/About";
+import About from "./components/About"
 import Technologies from "./components/Technologies";
 import Projects from "./components/Projects";
 import Education from "./components/Education";
 import Contact from "./components/Contact";
-
 function App() {
   return (
-    <div className="relative overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
-      
-      {/* Fondo que cubre todo el contenido */}
-      <div className="absolute top-0 left-0 -z-10 w-full h-full bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
-      
-      {/* Contenido principal */}
+    <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
+      <div className="flixed top-0 -z-10 h-full w-full">
+      <div className="fixed top-0 left-0 -z-10 h-full w-full bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+
+      </div>
+     {/* Fondo aplicado a toda la página */}
+     
       <div className="container mx-auto px-8">
-        <Navbar />
-        <Hero />
-        <About />
-        <Technologies />
-        <Projects />
-        <Education />
-        <Contact />
+      <Navbar />
+      <Hero/>
+      <About/>
+      <Technologies/>
+      <Projects/>
+      <Education/>
+      <Contact/>
       </div>
     </div>
   );
